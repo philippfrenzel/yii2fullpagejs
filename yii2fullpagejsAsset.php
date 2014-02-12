56ad2bfbbb4dae89222da7f3ad2fc87fa4591d79
Philipp Frenzel <philipp@frenzel.net>
2014-02-12 08:44:38 +0100

    initial upload

---
+++ yii2fullpagejsAsset.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.frenzel.net/
+ * @author Philipp Frenzel <philipp@frenzel.net> 
+ */
+
+namespace philippfrenzel\yii2fullpagejs;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class yii2fullpagejsAsset extends AssetBundle
+{
+    public $sourcePath = '@philippfrenzel/yii2fullpagejs/assets';
+    public $css = array(
+      'css/jquery.fullPage.css'
+    );
+    public $js = array(
+        'js/jquery.fullPage.js',
+        'vendors/jquery.slimscroll.min.js'
+    );
+    public $depends = array(
+        'yii\web\JqueryAsset',
+    );
+}
